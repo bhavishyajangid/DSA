0a61cb18908b82a641490ad1cd4fa59deb17a551
bhavishyajangid <bhavisya8824@gamil.com>
2025-07-13 19:57:38 +0530

    nw

--- linerOrbinarySearch.js
+++ linerOrbinarySearch.js
@@ -36,6 +36,73 @@
         
     }
  
-    console.log(findPair([5, 20, 3, 2, 5, 80] , 78));
+    // console.log(findPair([5, 20, 3, 2, 5, 80] , 78));
+
+
+    2. // Given an array of integers arr[] that is first strictly increasing and then maybe strictly decreasing, find the bitonic point, that is the maximum element in the array.
+// Bitonic Point is a point before which elements are strictly increasing and after which elements are strictly decreasing.
+
+// Note: It is guaranteed that the array contains exactly one bitonic point.
+
+   function findMaximum(arr) {
+        // your code here
+   let start = 0
+   let end = arr.length - 1
+   
+   
+   while(start < end){
+       let mid = Math.floor((start + end) / 2)
+       
+       
+       if(arr[mid] > arr[mid + 1] && arr[mid] > arr[end]){
+           end = mid
+       }else {
+           start = mid + 1
+       }
+       
+   }
+   return arr[start]
+    }
     
+    
+//  console.log(findMaximum([1,2,4,5,7,8,3]));
+ // here first we check if the mid element is greter than the next elemnt or greter then the end elemnt if yes then start from the end if not then increase the start and in the last return the arr[start] 
  
+
+  function findClosest(arr, k) {
+        // code here
+let start = 0;
+    let end = arr.length - 1;
+    let closest = arr[0]; // assume first element is closest initially
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        let currentDiff = Math.abs(arr[mid] - k);
+        let closestDiff = Math.abs(closest - k);
+
+        console.log(currentDiff ,closestDiff , closest ,arr[mid]);
+        
+
+        if (currentDiff < closestDiff) {
+            closest = arr[mid];
+        } 
+        // 💡 If same distance, prefer the **bigger** one
+        else if (currentDiff === closestDiff && arr[mid] > closest) {
+            closest = arr[mid];
+        }
+
+        if (arr[mid] < k) {
+            start = mid + 1;
+        } else if (arr[mid] > k) {
+            end = mid - 1;
+        } else {
+            return arr[mid]; // Exact match
+        }
+    }
+
+    return closest;
+    }
+
+    console.log(findClosest( [4, 5, 5, 6, 8, 12, 14, 16, 16, 19] , 10));
+    
