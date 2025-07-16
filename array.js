3c43f8f346c5bba4262efdff998f12b99347bdd6
bhavishyajangid <bhavishya8824@gmail.com>
2025-07-16 19:28:19 +0530

    array

--- array.js
+++ array.js
@@ -503,6 +503,43 @@
 //   }
 
 //   console.log(findDuplicate(arr));
+
+
+ 10. // Given an array arr[]. Find the majority element in the array. If no majority element exists, return -1.
+
+// Note: A majority element in an array is an element that appears strictly more than arr.size()/2 times in the array.
+
+// Examples:
+
+// Input: arr[] = [1, 1, 2, 1, 3, 5, 1]
+// Output: 1
+// Explanation: Since, 1 is present more than 7/2 times, so it is the majority element.
+
+function majorityElement(arr) {
+        // code here
+        let quantity = {}
+        
+        for(let i = 0 ; i < arr.length ; i++){
+             if(quantity[arr[i]]){
+                 quantity[arr[i]] = quantity[arr[i]] + 1
+             }else{
+                 quantity[arr[i]] = 1
+             }
+             
+        }
+
+
+         for(let j = 0 ; j < arr.length ; j++){  
+            if(quantity[arr[j]] > Math.floor(arr.length / 2)){
+                return arr[j]
+            }
+        }
+        
+        return -1
+    }
+
+    // console.log(majorityElement([1, 1, 2, 1, 3, 5, 1]));
+    
   
      
 
