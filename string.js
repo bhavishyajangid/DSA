e3565f4b89ad56ac79b41dbbcd333f4fd2ca6ce4
bhavishyajangid <bhavishya8824@gmail.com>
2025-08-06 19:25:55 +0530

    string

--- string.js
+++ string.js
@@ -464,3 +464,29 @@ function areAnagrams(s1, s2) {
     }
     // console.log(areAnagrams("allergy", "allergg"));
     
+ 12. //Given a string s in Roman number format, your task is to convert it to an integer. Various symbols and their values are given below.
+// Note: I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
+
+// Examples:
+
+// Input: s = "IX"
+// Output: 9
+// Explanation: IX is a Roman symbol which represents 10 – 1 = 9.
+// Input: s = "XL"
+// Output: 40
+// Explanation: XL is a Roman symbol which represents 50 – 10 = 40.
+// Input: s = "MCMIV"
+// Output: 1904
+// Explanation: M is 1000, CM is 1000 – 100 = 900, and IV is 4. So we have total as 1000 + 900 + 4 = 1904.
+// Constraints:
+
+function romanToDecimal(s) {
+        // code here
+        const map = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000};
+  let total = 0;
+  for(let i = 0; i < s.length; i++) {
+    if(map[s[i]] < map[s[i+1]]) total -= map[s[i]];
+    else total += map[s[i]];
+  }
+  return total
+    }
